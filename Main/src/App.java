fe61c7d2fdcfa0c5d18ce83024a0009e460618f7
erickxbliv <eriplayck@gmail.com>
2023-04-27 09:43:16 -0300

    bug import

--- Main/src/App.java
+++ Main/src/App.java
@@ -1,5 +1,6 @@
-import Roleta.java;
-public class App {
+import ClassesDeDados.Roleta;
+
+public class App{
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
